d9858bac85c2ad3eb3f23c5d0ddecfc10469e910
esoshyki <esoshyki@yandex.by>
2020-02-12 18:01:08 +0300

    init redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function playlist(state = [], action) {
+    if (action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+    return state
+}
+
+const store = createStore(playlist); 
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
+// import { createStore } from 'redux';
+
+
+
+// const store = createStore(playlist);
+// const addTrackButton = document.querySelectorAll('.addTrack')[0];
+// const list = document.querySelectorAll('.list')[0];
+// const trackInput = document.querySelectorAll('.textInput')[0];
+
+// store.subscribe(() => {
+//     list.innerHTML = null
+//     trackInput.value = null
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li)
+//     })
+// })
+
+// addTrackButton.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     store.dispatch({type: 'ADD_TRACK', payload: trackName})
+// })
